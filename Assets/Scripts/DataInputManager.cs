7375057045b1321e4f91d760b0e79941e4d1bf69
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-05-10 15:27:28 -0300

    Input field for text

---
+++ Assets/Scripts/DataInputManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUserName;
+   public void showGreetings()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+}
